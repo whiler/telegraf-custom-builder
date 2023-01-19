f71a77883a853dfe3b57a2150ba0420a6b90b76a
whiler <wenwu500@qq.com>
2023-01-19 12:44:43 +0800

    build multi arch with go

--- build.sh
+++ build.sh
@@ -6,6 +6,8 @@ REQ="$2"
 plugins="$(paste -sd ',' "${REQ}")"
 
 pushd "${SRC}" || exit 1
-	rm -f telegraf
-	BUILDTAGS="custom,${plugins}" LDFLAGS="-s -w" make
+	BUILDTAGS="custom,${plugins}" LDFLAGS="-s -w" GOARCH=amd64 make
+	mv telegraf telegraf.x86-64
+	BUILDTAGS="custom,${plugins}" LDFLAGS="-s -w" GOARCH=arm64 make
+	mv telegraf telegraf.aarch64
 popd || exit 1
